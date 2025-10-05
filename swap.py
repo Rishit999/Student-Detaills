cc8c1bb84c0dfd0f5aabb17a9b70faf867173dd6
Rishit999 <rishitnayakar@gmail.com>
2025-10-05 23:05:09 +0530

    saving the file to Main

---
+++ swap.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print(f"\nBefore Swapping: a is {a} and b is {b}")
+a,b = b,a
+print("\nAfter Swapping")
+print(f"a is {a} b is {b} ")
