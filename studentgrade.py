434f7b9dc8f1752a36b6204141d62338be880e7d
Rishit999 <rishitnayakar@gmail.com>
2025-11-10 15:34:11 +0530

    Saving the file to github

--- studentgrade.py
+++ studentgrade.py
@@ -1,12 +1,8 @@
-# student_grade.py
 
-# Marks for 5 subjects (predefined, since user input is not allowed)
-marks = [85, 78, 92, 67, 74]  # you can change these values
+marks = [85, 78, 92, 67, 74]  
 
-# Calculate average
 average = sum(marks) / len(marks)
 
-# Determine grade
 if average >= 85:
     grade = 'A'
 elif average >= 70:
